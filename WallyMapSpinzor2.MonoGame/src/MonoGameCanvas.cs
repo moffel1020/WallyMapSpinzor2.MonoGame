18f5b9fcb12fa190f1a07ae556ba54529303e98d
Eyal Zusiman <eyal.zusiman@gmail.com>
2023-11-18 17:19:08 +0200

    adjusted merged code

--- WallyMapSpinzor2.MonoGame/src/MonoGameCanvas.cs
+++ WallyMapSpinzor2.MonoGame/src/MonoGameCanvas.cs
@@ -11,7 +11,7 @@ public class MonoGameCanvas : ICanvas<Texture2DWrapper>
     public string BrawlPath{get; set;}
     public SpriteBatch Batch{get; set;}
     public BucketPriorityQueue<Action> DrawingQueue{get; set;} = new(Enum.GetValues<DrawPriorityEnum>().Length);
-    public Dictionary<string, Texture2DWrapper> TextureCache{get;} = new Dictionary<string, Texture2DWrapper>();
+    public Dictionary<string, Texture2DWrapper> TextureCache{get;} = new();
     public MonoGameCanvas(GraphicsDevice graphicsDevice, string brawlPath)
     {
         BrawlPath = brawlPath;
@@ -155,7 +155,7 @@ public class MonoGameCanvas : ICanvas<Texture2DWrapper>
         string finalPath = Path.Join(BrawlPath, "mapArt", path).ToString();
 
         TextureCache.TryGetValue(finalPath, out Texture2DWrapper? texture);
-        if (texture is not null) return texture;
+        if(texture is not null) return texture;
 
         texture = new(Texture2D.FromFile(Batch.GraphicsDevice, finalPath));
         TextureCache.Add(finalPath, texture);
